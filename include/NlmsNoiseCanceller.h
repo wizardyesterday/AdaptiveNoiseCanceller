21e35527a6968b3ec7b5b050143f77a4874a1445
Chris G <chris@saturn.earth>
2023-04-07 16:51:52 -0500

    We have a header file that is almost complete.

---
+++ include/NlmsNoiseCanceller.h
@@ -0,0 +1,58 @@
+//**************************************************************************
+// file name: NlmsNoiseCanceller.h
+//**************************************************************************
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// This class implements a signal processing block known as an adaptive
+// noise canceller.  A normalized LMS (least mean square) algorithm is
+// used for the coefficient update equation.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+#ifndef __NLMSNOISECANCELLER__
+#define __NLMSNOISECANCELLER__
+
+#include <stdint.h>
+
+class NlmsNoiseCanceller
+{
+  //***************************** operations **************************
+
+  public:
+
+  NlmsNoiseCanceller(int filterLength,int referenceDelay,float beta);
+  ~NlmsNoiseCanceller(void);
+
+  void acceptData(int16_t *bufferPtr,uint32_t bufferLength);
+  float filterData(float x);
+
+  private:
+
+  //*******************************************************************
+  // Utility functions.
+  //*******************************************************************
+  // Abstract the implementation of the pipeline.
+  void shiftSampleInPipeline(float x);
+
+  // This performs linear convolution.
+  float dotProduct(float a,float b,int n);
+
+  //*******************************************************************
+  // Attributes.
+  //*******************************************************************
+  // The number of taps in the filter.
+  int filterLength;
+ 
+  // The number of samples to delay the input data, x, so
+  //  that the reference signal, d(n) = x(n - n0), can be formed.
+  int referenceDelay;
+
+  // The adaptive filtering update (normalized step-size) parameter.
+  float beta;
+
+  // Pointer to the storage for the filter coefficients.
+  float *coefficientStoragePtr;
+
+  // Pointer to the filter state (previous samples).
+  float *filterStatePtr;
+};
+
+#endif // __NLMSNOISECANCELLER__
